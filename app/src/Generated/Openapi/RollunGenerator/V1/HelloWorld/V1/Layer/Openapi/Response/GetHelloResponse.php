7ad204594630e18d2ea3bcef74158d181a45dd90
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-09-27 16:28:00 +0300

    refactor hello-world: response object

--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Openapi/Response/GetHelloResponse.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Openapi/Response/GetHelloResponse.php
@@ -4,7 +4,48 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Response;
 
-interface GetHelloResponse
-{
+use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Dto\HelloWorldResponse;
+use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Dto\Problem;
 
+class GetHelloResponse
+{
+    private array $responses = [
+        200 => [
+            'content' => [
+                'application/vnd.rollun+json' => [
+                    'schema' => HelloWorldResponse::class
+                ]
+            ]
+        ],
+        400 => [
+            'content' => [
+                'application/problem+json' => [
+                    'schema' => Problem::class
+                ]
+            ]
+        ],
+        500 => [
+            'content' => [
+                'application/problem+json' => [
+                    'schema' => Problem::class
+                ]
+            ]
+        ],
+        503 => [
+            'content' => [
+                'application/problem+json' => [
+                    'schema' => Problem::class
+                ]
+            ]
+        ]
+    ];
+
+    public function __construct(
+        public readonly mixed $body,
+        public readonly int $statusCode,
+        public readonly array $headers = [],
+    )
+    {
+        // validate response is in $this->response
+    }
 }
