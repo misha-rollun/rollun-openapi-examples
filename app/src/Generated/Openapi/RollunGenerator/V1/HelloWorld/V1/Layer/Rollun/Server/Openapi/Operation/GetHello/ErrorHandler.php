d230245bd12b30969741786e01410c0b7303aaba
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-10-18 16:02:03 +0300

    feat hello-world: make error handler not abstract

--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Rollun/Server/Openapi/Operation/GetHello/ErrorHandler.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Rollun/Server/Openapi/Operation/GetHello/ErrorHandler.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Rollun\Server\Openapi\Operation\GetHello;
 
-abstract class ErrorHandler implements \Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Server\Http\Handler\GetHello\ErrorHandler
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+// TODO: implement returning application/problem+json response
+class ErrorHandler implements \Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Server\Http\Handler\GetHello\ErrorHandler
 {
-    // TODO: implement returning application/problem+json response
+    public function handleException(Throwable $e): ResponseInterface
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function handleValidationError(array $errors): ResponseInterface
+    {
+        throw new \Exception('Not implemented');
+    }
 }
