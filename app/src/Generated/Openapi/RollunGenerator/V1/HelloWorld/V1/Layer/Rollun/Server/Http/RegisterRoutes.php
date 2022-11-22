a14cf416cedadd7db4f908c3d3a208ef54b4651d
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-11-22 17:03:40 +0200

    feat hello-world: add RegisterRoutes implementation

---
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Rollun/Server/Http/RegisterRoutes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Rollun\Server\Http;
+
+class RegisterRoutes extends \Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Http\Server\RegisterRoutes
+{
+    public function register(): void
+    {
+        throw new \Exception('Not implemented');
+    }
+}
