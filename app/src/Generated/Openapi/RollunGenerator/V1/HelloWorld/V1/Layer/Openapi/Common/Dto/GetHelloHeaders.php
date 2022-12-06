332efa4b40506673d4439860895568db7f0e758c
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-12-06 18:43:17 +0200

    refactor hello-world: response struct

---
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Openapi/Common/Dto/GetHelloHeaders.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Common\Dto;
+
+class GetHelloHeaders
+{
+    /**
+     * Генерується завжди, незалежно від того чи вказаний він в маніфесті
+     */
+    public string $contentType;
+}
--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Openapi/Common/Operation/GetHello/Response.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Layer/Openapi/Common/Operation/GetHello/Response.php
@@ -4,53 +4,16 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Common\Operation\GetHello;
 
+use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Common\Dto\GetHelloHeaders;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Common\Dto\HelloWorldResponse;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Layer\Openapi\Common\Dto\Problem;
 
 class Response
 {
-    private array $responses = [
-        200 => [
-            'content' => [
-                'application/vnd.rollun+json' => [
-                    'schema' => HelloWorldResponse::class
-                ]
-            ]
-        ],
-        400 => [
-            'content' => [
-                'application/problem+json' => [
-                    'schema' => Problem::class
-                ]
-            ]
-        ],
-        500 => [
-            'content' => [
-                'application/problem+json' => [
-                    'schema' => Problem::class
-                ]
-            ]
-        ],
-        503 => [
-            'content' => [
-                'application/problem+json' => [
-                    'schema' => Problem::class
-                ]
-            ]
-        ]
-    ];
-
-    public function __construct(
-        public readonly mixed $body,
-        public readonly int $statusCode,
-        public readonly array $headers = [],
-    )
-    {
-        // validate response is in $this->response
-    }
-
-    public function getContentType(): string
-    {
-        return $this->headers['Content-Type'];
-    }
+    /**
+     * В тайп хінті перераховані усі можливі варіанти body
+     */
+    public HelloWorldResponse|Problem $body;
+    public int $statusCode;
+    public GetHelloHeaders $headers;
 }
