177c9fc37324e8473d41daec6f64ca146965fc5c
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2022-12-17 18:09:27 +0200

    feat hello-world: implement Rollun client Request and Response Converters

--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Infrastructure/Rollun/Client/Handler/GetHello/RequestConverter.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Infrastructure/Rollun/Client/Handler/GetHello/RequestConverter.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Rollun\Client\Handler\GetHello;
 
+use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Openapi\Common\Dto\GetHelloRequestHeaders;
+use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Openapi\Common\Operation\GetHello\Request as OpenapiRequest;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Rollun\Common\Handler\GetHello\Request;
 
 class RequestConverter
 {
-    public function convert(Request $request): \Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Openapi\Common\Operation\GetHello\Request
+    public function convert(Request $request): OpenapiRequest
     {
-        throw new \Exception('Not implemented.');
+        $openapiRequest = new OpenapiRequest();
+        $openapiRequest->headers = new GetHelloRequestHeaders();
+        $openapiRequest->headers->contentType = 'application/vnd.rollun-request+json';
+        $openapiRequest->queryParameters = $request->query;
+        return $openapiRequest;
     }
 }
--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Infrastructure/Rollun/Client/Handler/GetHello/ResponseConverter.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Infrastructure/Rollun/Client/Handler/GetHello/ResponseConverter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Rollun\Client\Handler\GetHello;
 
+use Generated\Openapi\RollunGenerator\V1\Common\Infrastructure\Rollun\Handler\Status;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Openapi\Common\Operation\GetHello\Response;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Infrastructure\Rollun\Common\Handler\GetHello\Result;
 
@@ -11,6 +12,26 @@ class ResponseConverter
 {
     public function convert(Response $response): Result
     {
-        throw new \Exception('Not implemented.');
+        if ($response->statusCode === 200) {
+            return new Result(
+                Status::fulfilled(),
+                $response->body,
+            );
+        } elseif ($response->statusCode == 202) {
+            return new Result(
+                Status::pending(),
+                null,
+                null,
+                $response->headers->resourceId
+            );
+        } elseif ($response->statusCode >= 400 && $response->statusCode <= 599) {
+            return new Result(
+                Status::rejected(),
+                null,
+                $response->body
+            );
+        } else {
+            throw new \RuntimeException('Unsupported status code.');
+        }
     }
 }
