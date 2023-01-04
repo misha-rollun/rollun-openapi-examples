14a0497d63af947b8eb4c2f365cc26be7fdbbb42
Zuiev Mykhailo <zuiev.rollun@gmail.com>
2023-01-04 20:04:49 +0200

    feat hello-world: add verror handling

--- app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Server/Handler/GetHello/Handler.php
+++ app/src/Generated/Openapi/RollunGenerator/V1/HelloWorld/V1/Server/Handler/GetHello/Handler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Server\Handler\GetHello;
 
+use Generated\Openapi\RollunGenerator\V1\Common\Utils\ErrorHandler\ErrorHandler;
 use Generated\Openapi\RollunGenerator\V1\HelloWorld\V1\Server\Controller\GetHello\AbstractController;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,6 +13,7 @@ use Psr\Http\Server\RequestHandlerInterface;
 class Handler implements RequestHandlerInterface
 {
     public function __construct(
+        private ErrorHandler $errorHandler,
         private RequestValidator $requestValidator,
         private RequestConverter $requestConverter,
         private AbstractController $controller,
@@ -23,11 +25,15 @@ class Handler implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $this->requestValidator->validate($request);
-        $openapiRequest = $this->requestConverter->convert($request);
-        $openapiResponse = $this->controller->handle($openapiRequest);
-        $response = $this->responseConverter->convert($openapiResponse);
-        $this->responseValidator->validate($response);
-        return $response;
+        try {
+            $this->requestValidator->validate($request);
+            $openapiRequest = $this->requestConverter->convert($request);
+            $openapiResponse = $this->controller->handle($openapiRequest);
+            $response = $this->responseConverter->convert($openapiResponse);
+            $this->responseValidator->validate($response);
+            return $response;
+        } catch (\Throwable $e) {
+            return $this->errorHandler->handle($e);
+        }
     }
 }
